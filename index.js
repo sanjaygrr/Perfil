11f05e8593f6dfb19805e98d18827bca564fa875
sanjaygrr <sanjay.ramchandani@triciclos.cl>
2024-05-17 18:53:31 -0400

    feat  add toggle menu

--- index.js
+++ index.js
@@ -347,4 +347,11 @@ document.addEventListener('DOMContentLoaded', function () {
             }
         });
     });
+
+    const navToggle = document.getElementById('nav-toggle');
+    const navMenu = document.getElementById('nav-menu');
+
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.toggle('show');
+    });
 });
